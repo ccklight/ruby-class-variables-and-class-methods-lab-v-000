9e0d681a0e9cee6333abd9d326d287fa03094c5a
Cheryl K <cherylkelson@icloud.com>
2018-09-25 13:28:58 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,49 +20,4 @@ class Song
  end
 
   
-  Song.count
-  @@count = 0 
-  def initialize
-    @@album_count += 1
-  end 
-  def self.count 
-      @@album_count
-  end
-    
-  
-  
-  Song.artists
-  Song.artist_count
-    Song.artist_count += 1 
-    
-    
-  class. Count 
-    return
-  Song.count  
-  
-  class.genres 
-  class.genres = [genres].first.authenticate_or_request_with_http_digest
-  @@genres += 1 
-  
-  
-  .artists = []
-  return [artists].first 
-  #use loop or each.do Or Iterate.Yes, Cheryl you got that right!
-  .each do ||
-    
-    
-    
-    Song.genre_count
-    
-    returns {k,v}
-    returns [names, count]
-    
-    
-  class  @@genres = []
-  
-  
-  
-  Song.genres 
-  #Use a hash ?
-  Song.genre_count
   
