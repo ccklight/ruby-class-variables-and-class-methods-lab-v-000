d4b1011aade3127dcd55b65b6e9ff31db38d9182
Cheryl K <cherylkelson@icloud.com>
2018-09-25 14:12:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,6 +45,18 @@ class Song
   
  
   def self.artist_count
-    
+    artist_count = {} 
+    @@artist_count.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else 
+        artist_count[artist] = 1 
+        end 
+      end 
+      
+      artist_count 
+  end 
+  
+end 
   end
   
