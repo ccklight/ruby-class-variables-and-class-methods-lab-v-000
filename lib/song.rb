3fe68d54d0eb8a5ea0da7a22318f2c847fbdf979
Cheryl K <cherylkelson@icloud.com>
2018-09-25 13:21:09 +0000

    automatically backed up by learn

---
+++ lib/song.rb
@@ -0,0 +1,80 @@
+class Song 
+  def initialize (name, artist, genre)
+  attr_accessor:name, :artist, :genre
+  
+ @@count = 0
+ @@artists = []
+ @@genres = []
+
+
+ def initialize(name, artist, genre)
+   @name = name
+   @artist = artist
+   @genre = genre
+   @@count +=1
+   @@genres << genre
+   @@artists << artist
+ end
+
+
+
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  Song.count
+  @@count = 0 
+  def initialize
+    @@album_count += 1
+  end 
+  def self.count 
+      @@album_count
+  end
+    
+  
+  
+  Song.artists
+  Song.artist_count
+    Song.artist_count += 1 
+    
+    
+  class. Count 
+    return
+  Song.count  
+  
+  class.genres 
+  class.genres = [genres].first.authenticate_or_request_with_http_digest
+  @@genres += 1 
+  
+  
+  .artists = []
+  return [artists].first 
+  #use loop or each.do Or Iterate.Yes, Cheryl you got that right!
+  .each do ||
+    
+    
+    
+    Song.genre_count
+    
+    returns {k,v}
+    returns [names, count]
+    
+    
+  class  @@genres = []
+  
+  
+  
+  Song.genres 
+  #Use a hash ?
+  Song.genre_count
+  
