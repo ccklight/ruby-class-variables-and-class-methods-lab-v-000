05631b22ab72747a7ac14bd256750fc4cb421e9c
Cheryl K <cherylkelson@icloud.com>
2018-09-25 13:32:17 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,21 +2,14 @@ class Song
   
   attr_accessor:name, :artist, :genre
   
- @@count = 0
+  @@count = 0
  
- def initialize 
-   @@count += 1 
+  def initialize 
+  @@count += 1 
    
   def self.count
     @@count  
   end
    
- @@count = 0
- @@artists = []
- @@genres = []
-
-
  
-
-  
   
