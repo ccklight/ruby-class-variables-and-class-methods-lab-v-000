db296093afdd649dc10574a8025731fdf6558177
Cheryl K <cherylkelson@icloud.com>
2018-09-25 14:20:24 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -31,7 +31,7 @@ class Song
   
   def self.genre_count
     genre_count = {}
-    @@genre_count.each do |genre| 
+    @@genres.each do |genre| 
       if genre_count[genre]
         genre_count[genre] += 1 
       else 
